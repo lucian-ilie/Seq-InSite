0e14d9374d98bed216d05d77d7c7ad2b31ed228c
mohsenh17 <80420551+mohsenh17@users.noreply.github.com>
2024-01-14 11:55:50 -0500

    Update Seq-InSite.sh
    fixing path

--- Seq-InSite.sh
+++ Seq-InSite.sh
@@ -17,7 +17,7 @@ embd=$User_Dir/testFeatures/embd/
 t5Embd=$User_Dir/testFeatures/t5Embd/
 output=$User_Dir/output/
 # Create a file for each protein in fasta file
-python Utils/fastaToFile.py $DATASET_FILE $fastaDir
+python Utils/fastaToFiles.py $DATASET_FILE $fastaDir
 
 # Compute MSA file for each protein
 bash Utils/gra3m.sh $fastaDir $a3mDir
@@ -27,4 +27,4 @@ python Utils/T5Embd.py $DATASET_FILE $t5Embd
 python Utils/msaEmbd.py $DATASET_FILE $a3mDir $embd
 
 # Predict Seq-InSite 
-python Architecture/predict_ENS.py $DATASET_FILE $embd $t5Embd $output
+python Architectures/predict_ENS.py $DATASET_FILE $embd $t5Embd $output
